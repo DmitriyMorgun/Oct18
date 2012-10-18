e879e20ead8070190499f77a777ccb56419b6b9e
Dmitriy Morgun <dmitriy.morgun@gmail.com>
2012-10-17 20:26:38 -0400

    3rd commit

---
+++ Oct18/Oct18AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Oct18AppDelegate.h
+//  Oct18
+//
+//  Created by Dmitriy Morgun on 10/17/12.
+//  Copyright (c) 2012 Dmitriy Morgun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Oct18AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
